8bd3901017ee14063ad97915db49e64424630c32
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2023-02-05 20:16:12 +0200

    final proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LayoutComponent from "./components/layout/LayoutComponent";
 import Header from "./components/navbar/Header";
 import "./App.scss";
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div>
         <Header />
         <Routes>
-          <Route path="/" element={<LayoutComponent />} />
+          <Route path="/stvdy" element={<LayoutComponent />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/stvdy",
+  build: {
+    chunkSizeWarningLimit: 2800,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        "@assets": path.resolve(__dirname, './src/assets'),
+      }]
+  }
+
 })
